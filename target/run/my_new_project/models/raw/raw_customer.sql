6e13e0e49540a0d3bf2ff25a1f93cf22d6329283
sristi.patel2013@gmail.com <sristi.patel2013@gmail.com>
2022-08-30 07:07:18 +0000

    manifest json file

--- target/run/my_new_project/models/raw/raw_customer.sql
+++
@@ -1,9 +0,0 @@
-
-
-      create or replace transient table raw.dbt_spatel.raw_customer  as
-      (
-select * 
-from 
-raw.globalmart.customer
-      );
-    
--- target/run/my_new_project/models/raw/raw_order.sql
+++
@@ -1,9 +0,0 @@
-
-  create or replace  view raw.dbt_spatel.raw_order
-  
-   as (
-    
-select * 
-from
-raw.globalmart.orders
-  );
